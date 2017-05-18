6f4faa869d9050abea029b7181589627cf6d60d1
admin <admin@admin.local>
2017-05-18 17:11:47 +0800

    验证邮箱手机号等

---
+++ BeyondArray/CheckString/NSString+STRegex.h
@@ -0,0 +1,16 @@
+//
+//  NSString+STRegex.h
+//  BeyondArray
+//
+//  Created by admin on 2017/5/18.
+//  Copyright © 2017年 admin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (STRegex)
+
+
+
+@end
+
